a93bd264e12cae69ffed003394c1d251a0b11d08
FlippantPope <jonpmiddleton@gmail.com>
2025-05-31 01:33:43 -0700

    Added breadcrumb for dashboard navigation

--- src/routes/(dashboard)/admin/[tenant_slug]/+page.svelte
+++ src/routes/(dashboard)/admin/[tenant_slug]/+page.svelte
@@ -4,17 +4,19 @@ const { data } = $props();
 const tenantSlug = $derived(data.tenant.slug);
 const tenantName = $derived(data.tenant.name);
 const forms = $derived(data.forms);
-$inspect(data);
 </script>
-<h1>{tenantName}</h1>
-<div>Admin Page</div>
-<div>
+<div class="px-4 py-3">
+  <h1 class="text-xl">{tenantName} Dashboard</h1>
+  <!--<div>Admin Page</div>-->
+  <div>
     {#each forms as survey}
-        <div class="border-b my-2 py-3">
-        <h2>{survey.title}</h2>
-        <p>{survey.description}</p>
-            <a href={`/survey/${survey.slug}`}>Take Survey</a>
-            <a href={`${tenantSlug}/forms/${survey.slug}`}>Edit</a>
-        </div>
+      <div class="border-b my-2 py-3">
+        <h2 class="text-lg">{survey.title}</h2>
+        <p class="text-muted-foreground">{survey.description}</p>
+        <p><a class="text-sm hover:underline" href={`/survey/${survey.slug}`}>Take Survey</a></p>
+        <p><a class="text-sm hover:underline" href={`${tenantSlug}/forms/${survey.slug}/results`}>View Results</a></p>
+        <p><a class="text-sm hover:underline" href={`${tenantSlug}/forms/${survey.slug}`}>Edit</a></p>
+      </div>
     {/each}
+  </div>
 </div>
